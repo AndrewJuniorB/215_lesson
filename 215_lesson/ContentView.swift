f298796b3fc0ced56486eab9790919959f2b575d
AndrewJunior <andrey.i.burlakov@gmail.com>
2020-01-28 20:47:50 -0800

    Initial Commit

---
+++ 215_lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  215_lesson
+//
+//  Created by Andrew Burlakov on 1/28/20.
+//  Copyright © 2020 Andrew Burlakov junior. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
